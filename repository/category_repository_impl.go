5837b2a4baaeebdce9c8341c82689eed4ad867fc
danimisme <msubhanr53@gmail.com>
2026-08-08 19:51:18 +0700

    feat: implement category repository interface with placeholder methods

---
+++ repository/category_repository_impl.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"belajar-golang-restful-api/model/domain"
+	"context"
+	"database/sql"
+)
+
+type CategoryRepositoryImpl struct {
+
+}
+
+	func (repository *CategoryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
+		panic("Implement me")
+	}
+
+	func (repository *CategoryRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
+		panic("Implement me")
+	}
+
+	func (repository *CategoryRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, category domain.Category) {
+		panic("Implement me")
+	}
+
+	func (repository *CategoryRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, categoryId int) domain.Category {
+		panic("Implement me")
+	}
+
+	func (repository *CategoryRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) []domain.Category {
+		panic("Implement me")
+	}
