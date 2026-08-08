9604f11c29860fc1bcf470c7e834230b62e9587e
danimisme <msubhanr53@gmail.com>
2026-08-09 01:36:57 +0700

    feat: add validation error handling to the global error handler

--- exeption/error_handler.go
+++ exeption/error_handler.go
@@ -4,14 +4,37 @@ import (
 	"belajar-golang-restful-api/helper"
 	"belajar-golang-restful-api/model/web"
 	"net/http"
+
+	"github.com/go-playground/validator"
 )
 
 func ErrorHandler(writter http.ResponseWriter, request *http.Request, err interface{}){
 	if notFoundError(writter, request, err) {
 		return
 	}
+
+	if validationError(writter, request, err) {
+		return
+	}
+	
 	InternalServerError(writter, request, err)
 	
+}
+
+func validationError(writter http.ResponseWriter, request *http.Request, err interface{}) bool{
+	exception, ok := err.(validator.ValidationErrors)
+	if ok {
+		writter.Header().Set("Content-Type", "application/json")
+		writter.WriteHeader(http.StatusBadRequest)
+		webResponse := web.WebResponse{
+			Code: http.StatusBadRequest,
+			Status: "Bad Request",
+			Data: exception.Error(),
+		}
+		helper.WriteToResponseBody(writter, webResponse)
+		return true
+	}
+	return false
 }
 
 func notFoundError(writter http.ResponseWriter, request *http.Request, err interface{}) bool{
