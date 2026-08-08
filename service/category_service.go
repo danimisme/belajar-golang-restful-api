1913ca671f32b3201c8840f886bda482a6e1a608
danimisme <msubhanr53@gmail.com>
2026-08-08 20:37:32 +0700

    feat: add CategoryUpdateRequest model and define CRUD method signatures in CategoryService

--- service/category_service.go
+++ service/category_service.go
@@ -7,4 +7,8 @@ import (
 
 type CategoryService interface {
 	Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse
+	Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse
+	Delete(ctx context.Context)
+	FindById(ctx context.Context)
+	FindAll(ctx context.Context)
 }
