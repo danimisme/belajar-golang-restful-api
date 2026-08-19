5b3a47bcf77e5090768963a28d09a2765cb41c19
danimisme <msubhanr53@gmail.com>
2026-08-19 14:57:25 +0700

    feat(router): add swagger ui endpoint

--- app/router.go
+++ app/router.go
@@ -3,8 +3,10 @@ package app
 import (
 	"belajar-golang-restful-api/controller"
 	"belajar-golang-restful-api/exeption"
+	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 func NewRouter(categoryController controller.CategoryController) *httprouter.Router {
@@ -15,6 +17,9 @@ func NewRouter(categoryController controller.CategoryController) *httprouter.Rou
 	router.POST("/api/categories", categoryController.Create)
 	router.PUT("/api/categories/:categoryId", categoryController.Update)
 	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+	router.GET("/swagger/*any", func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		httpSwagger.WrapHandler.ServeHTTP(w, r)
+	})
 
 	router.PanicHandler = exeption.ErrorHandler
 
